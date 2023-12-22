94b98636ed95f16e8920f516dbdb25dbc255aff4
Andrada QM <andrada.mq@gmail.com>
2023-12-22 23:09:32 +0100

    prepare index for validation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-//const hamburger = document.querySelector('.hamburger');
-//const navLink = document.querySelector('.nav_link');
-
-//hamburger.addEventListener('click', () => {
-//  navLink.classList.toggle('hide');
-//});
-
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-//window.onscroll = function() {scrollFunction()};
-
-//function scrollFunction() {
-//  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//    document.getElementById("banner").style.display = "none";
-//  } }
-
 window.addEventListener('DOMContentLoaded', event => {
 
   const sidebarWrapper = document.getElementById('sidebar-wrapper');
